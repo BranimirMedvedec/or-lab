fa4338289616aa93ff11d0c88b35a791e86094a6
BranimirMedvedec <94074954+BranimirMedvedec@users.noreply.github.com>
2023-11-13 18:09:20 +0100

    add bat to fill db

---
+++ popuniBazu.bat
@@ -0,0 +1,1 @@
+docker exec -u root or-db mongoimport --db=orlabDB --collection=clubs --type=json --username=bmedvedec --password=lozinka --authenticationDatabase=admin --file=sportski_klubovi.json --jsonArray
