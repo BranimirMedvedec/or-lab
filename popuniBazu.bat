3ca28832b0fcd02d7c70409bc2af831c80434d60
Branimir Medvedec <94074954+BranimirMedvedec@users.noreply.github.com>
2023-12-18 16:38:18 +0100

    or lab3 commit

--- popuniBazu.bat
+++ popuniBazu.bat
@@ -1,1 +1,2 @@
-docker exec -u root or-db mongoimport --db=orlabDB --collection=clubs --type=json --username=bmedvedec --password=lozinka --authenticationDatabase=admin --file=sportski_klubovi.json --jsonArray
+docker exec -u root or-db mongoimport --db=orlabDB --collection=clubs --type=json --username=bmedvedec --password=lozinka --authenticationDatabase=admin --file=clubs.json --jsonArray
+docker exec -u root or-db mongoimport --db=orlabDB --collection=api --type=json --username=bmedvedec --password=lozinka --authenticationDatabase=admin --file=openapi.json
